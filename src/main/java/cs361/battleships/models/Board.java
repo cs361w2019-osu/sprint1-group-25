56060e69d169d98507534160ec9e05df3354500b
Sean B. Higgins <higgisea@oregonstate.edu>
2019-01-18 17:50:37 -0800

    Created the loop that will fill the board with unique squares. Stores the created squares in a list of squares.

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -1,15 +1,31 @@
 package cs361.battleships.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List;;
 
 public class Board {
+
+	private int maxRows = 10;
+	private int maxCols = 10;
+
+	@JsonProperty private List<Square> boardSquares;
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Board() {
 		// TODO Implement
+		// Create a bunch of new square objects via loop, and store them in a list.
+
+		for (int i = 1; i <= maxRows; i++) {
+			for (int j = 0; j < maxCols; j++) {
+				Square loopSquare = new Square(i, (char)(j + 65));
+				this.boardSquares.add(loopSquare);
+			}
+		}
+
 	}
 
 	/*
@@ -44,5 +60,9 @@ public class Board {
 
 	public void setAttacks(List<Result> attacks) {
 		//TODO implement
+	}
+
+	public List<Square> getBoardSquares() {
+		return boardSquares;
 	}
 }
